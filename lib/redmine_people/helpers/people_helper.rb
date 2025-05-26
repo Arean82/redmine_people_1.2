73ce7dbffb7f87230b60c83f9312e01ef3567846
arean82 <arean82@gmail.com>
2025-05-26 17:27:27 +0530

    updated people helper

--- lib/redmine_people/helpers/people_helper.rb
+++ lib/redmine_people/helpers/people_helper.rb
@@ -2,14 +2,15 @@
 
 module RedminePeople
   module Helper
+    module PeopleHelper
 
-    def department_tree_tag(person, options={})
-      return "" if person.department.blank?
-      person.department.self_and_ancestors.map do |department|
-        link_to department.name, department_path(department.id, options)
-      end.join(' &#187; ').html_safe
+      def department_tree_tag(person, options={})
+        return "" if person.department.blank?
+        person.department.self_and_ancestors.map do |department|
+          link_to department.name, department_path(department.id, options)
+        end.join(' &#187; ').html_safe
+      end
     end
-
   end
 end
 
