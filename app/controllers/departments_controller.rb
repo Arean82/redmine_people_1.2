316254bef33dee05fc96244dab2e83cb7f8b4aec
arean82 <38061300+Arean82@users.noreply.github.com>
2025-05-26 13:06:47 +0530

    Update departments_controller.rb

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,29 +1,16 @@
-# This file is a part of Redmine CRM (redmine_contacts) plugin,
-# customer relationship management plugin for Redmine
-#
-# Copyright (C) 2011-2016 Kirill Bezrukov
-# http://www.redminecrm.com/
-#
-# redmine_people is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# redmine_people is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with redmine_people.  If not, see <http://www.gnu.org/licenses/>.
 
 class DepartmentsController < ApplicationController
   unloadable
 
-  before_filter :find_department, :except => [:index, :create, :new]
-  before_filter :require_admin, :only => [:destroy, :new, :create]
-  before_filter :authorize_people, :only => [:update, :edit, :add_people, :remove_person]
-  before_filter :load_department_events, :load_department_attachments, :only => [:show, :load_tab]
+  # before_filter :find_department, :except => [:index, :create, :new]
+  # before_filter :require_admin, :only => [:destroy, :new, :create]
+  # before_filter :authorize_people, :only => [:update, :edit, :add_people, :remove_person]
+  # before_filter :load_department_events, :load_department_attachments, :only => [:show, :load_tab]
+
+  before_action :find_department, except: [:index, :create, :new]
+  before_action :require_admin, only: [:destroy, :new, :create]
+  before_action :authorize_people, only: [:update, :edit, :add_people, :remove_person]
+  before_action :load_department_events, :load_department_attachments, only: [:show, :load_tab]
 
   helper :attachments
 
